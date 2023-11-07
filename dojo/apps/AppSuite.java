04e7705e7fbcf82b989db11852503652e651307b
Sosthene Grosset-Janin <sgrosset@gmail.com>
2023-11-07 14:28:21 +0100

    create Appsuite.java class

---
+++ dojo/apps/AppSuite.java
@@ -0,0 +1,61 @@
+package dojo.apps;
+
+
+/**
+ * Primary Class: AppSuite
+*/
+/**
+* Summary: AppSuite Class
+*
+* @author  (Sosthene Grosset-Janin & Juninho Loungila)
+* @version (1.0)
+* @since   Novmeber 2023
+*/
+public class AppSuite 
+{
+    // Constants
+        // Static Constants
+        // Non-Static Constants
+    // Variables    
+        // Declarations
+            // Primitives
+            // Objects
+        // Intialisations    
+    // Logic
+        // Constructors
+            public AppSuite()
+            {
+                // Constants
+                    // Static Constants
+                        String welcomeMessage = "WELCOME TO THIS APP";
+                        String farewellMessage = "THANK YOU FOR USING THIS APP";
+                    // Non-Static Constants
+                // Variables    
+                    // Declarations
+                        // Primitives
+                        // Objects
+                    // Intialisations    
+                // Logic
+                    // Welcome
+                    spalshScreen(welcomeMessage);
+                    // Show User Interface
+                    // Show Menu
+                    // Show User Interface
+                    // Farewell
+                    spalshScreen(farewellMessage);
+            }
+        // Getters & Setters
+        // Helpers
+        // Utilities
+            public void spalshScreen(String someMessage)
+            {
+                System.out.println(someMessage);
+            }
+}
+
+
+// #### Utility Classes
+
+/**
+ * Utility Class #1: 
+*/
