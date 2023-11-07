e7c57fa90a54b4e9c7a74d3f9d4f00f535170c5d
Juninho Loungila <jloungila@gmail.com>
2023-11-07 14:25:07 +0100

    created and Documented Class Runner

---
+++ dojo/src/Runner.java
@@ -0,0 +1,31 @@
+public class Runner
+{
+    // Variables
+        // Primitives
+        // Objets
+    // Function
+        // Constructor
+            public Runner()
+            {
+               // Variables
+                    // Primitives
+                    // Objets
+               // LOgic
+                  // Inputs
+                  // Processing
+                  // Validations
+                  // Exceptions
+                  // Outputs
+                  // Returns    
+            }
+
+    // Main Function
+    public static void main ()
+    {
+        // Variables
+            // Primitives
+            // Objects
+            Runner myRunner = New Runner();
+
+    }
+}   
